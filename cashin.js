8d68a65b1b79af5accfff0a6841134cdd231dfb2
DanielGals <janjangallaron@gmail.com>
2023-09-02 23:48:58 +0800

    Added Cash in working properly

--- cashin.js
+++ cashin.js
@@ -28,6 +28,8 @@ let storedValueWithoutCommas = '';
   const confirmButton = document.getElementById('confirmButton');
   const confirmButton2 = document.getElementById('confirmButton2');
 
+  
+
   confirmButton.addEventListener('click', async function() 
   {
     innerBox.style.display = 'none';
@@ -39,7 +41,8 @@ let storedValueWithoutCommas = '';
     const CashInAmount = storedValueWithoutCommas; 
     console.log(CashInAmount); // Now you can use valueToStore as needed
 
-    const OTP = 123456;
+    const OTP = generateRandomSixDigits();
+    console.log(OTP);
 
 
     const email = await checkEmail();
@@ -64,25 +67,62 @@ let storedValueWithoutCommas = '';
   const confirmButton2 = document.getElementById("confirmButton2");
   const otpInput = document.querySelector(".otp-input");
 
-  confirmButton2.addEventListener("click", function() {
+  const success = document.getElementById('success-page');
+
+
+  confirmButton2.addEventListener("click", async function() {
       const enteredOTP = otpInput.value;
 
       if (enteredOTP == OTP) {
           // Do something with the enteredOTP variable, like sending it for verification
           console.log("CORRECT");
+          
+          otpPage.style.display = 'none';
+          success.style.display = 'block';
+
+          const TransID = await checkAccountAgain(); //Return Trans ID
+          console.log(TransID + "received ko na final na talga");
+
+          //DATABASE CREATED
+          //Create API endpoint
+          
+          console.log(CashInAmount + " Amount ng cash in");
+          insertBalanceOnFrontend(TransID, CashInAmount);
+
       } else {
-          alert("Please enter a valid 6-digit OTP.");
+          const errorMessage = document.getElementById('error-message');
+          errorMessage.textContent = 'Invalid OTP Code';
+          errorMessage.style.display = 'block';
+          throw new Error('Invalid email or password');
       }
   });
-  
-  
   });
 
-  confirmButton2.addEventListener('click', () => {
-    // Handle the confirmation logic for the OTP-Page
+  
+  async function insertBalanceOnFrontend(accountId, amountToAdd) {
+    try {
+      const response = await fetch('http://localhost:8080/insert-balance', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ accountId, amountToAdd }),
+      });
+  
+      if (!response.ok) {
+        // Handle error responses (e.g., server error)
+        throw new Error('Failed to insert balance');
+      }
+  
+      const data = await response.json();
+      return data.newBalance; // This should contain the new balance returned by the server
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
     
-    
-  });
+  // });
 
 // TO DISPLAY USERNAME
 function UsernameText(username) {
@@ -142,3 +182,61 @@ async function checkEmail() {
   }
 }
   //
+
+  function generateRandomSixDigits() {
+    return Math.floor(100000 + Math.random() * 900000);
+  }
+
+
+
+  const apiUrl = 'https://zenbank-server.vercel.app';
+  async function getTransID(user_id) {
+    try {
+        const response = await fetch(`${apiUrl}/accounts/${user_id}`);
+        const userData = await response.json();
+
+        if (userData && userData.balance !== undefined) {
+
+            console.log('Eto yung final answer' + userData.account_id);
+            return userData.account_id;
+        } else {
+            balanceElement.textContent = "Balance information not available";
+            accountNum.textContent = "Account NUmber not available";
+        }
+    } catch (error) {
+        console.error("Error fetching user data:", error);
+        balanceElement.textContent = "Error fetching balance";
+    }
+}
+
+ async function checkAccountAgain() {
+    const accessToken = localStorage.getItem('ACCESS_TOKEN');
+    console.log(localStorage);
+    try {
+      // const accessToken = tokenHandler.getToken();
+      const response = await fetch('https://zenbank-server.vercel.app/check-token', {
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${accessToken}`, // The password to access the API
+        },
+      });
+  
+      if (!response.ok) {
+        throw new Error('Network response was not ok.');
+      }
+  
+      const data = await response.json();
+      
+     
+
+      //CALL Fetch Balance HERE
+      const userID = data[0].user_id;
+      console.log(userID, " USER ID");
+      console.log(data[0].username);
+      return await getTransID(userID);
+      
+    } catch (error) {
+      // Handle errors
+    }
+  }
+  
